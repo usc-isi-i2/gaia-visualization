68e243ade35f1c450d2005633ffa089d946e859a
Shinyi Chen <jchen@isi.edu>
2019-04-09 19:14:05 -0700

    Gaia0304

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
-repo = 'gaia0404ta1-test'
+repo = 'gaia0304ta1-test'
 gt_file = 'groundtruth.jl'
-debug_file = 'clusters-debug-gaia0404.jl'
-port = '5055'
+debug_file = 'clusters-debug-gaia0304.jl'
+port = '5050'
 
 endpoint = 'http://gaiadev01.isi.edu:7200/repositories/' + repo
 wikidata_endpoint = "http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql"
