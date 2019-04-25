5667a7973f1ac915274cb8eca445a48b045f935a
Shinyi Chen <jchen@isi.edu>
2019-04-25 11:32:00 -0700

    add more repository

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@ endpoint = 'http://gaiadev01.isi.edu:7200/repositories'
 # wikidata_endpoint = "http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql"
 wikidata_endpoint = 'https://query.wikidata.org/sparql'
 store_data = 'store_data'
-repositories = ['gaia0304ta1-test', 'gaia0404ta1-test', 'gaia0404ta1-testing', 'jchen-test']
+repositories = ['gaia0304ta1-test', 'gaia0404ta1-test', 'gaia0404ta1-testing', 'jchen-test', 'validation-test']
 username = 'admin'
 password = 'gaia@isi'
 
