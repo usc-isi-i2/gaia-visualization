7777a930421a489f64e9f181858f7d4c1d13b65d
Shinyi Chen <jchen@isi.edu>
2019-04-25 09:47:35 -0700

    Remove unsed file and clean up settings

--- setting.py
+++ setting.py
@@ -1,19 +1,15 @@
-repo = 'gaia0404ta1-testing'
-gt_file = 'groundtruth.jl'
-port = '5050'
-
+port = '5000'
 endpoint = 'http://gaiadev01.isi.edu:7200/repositories'
-endpoint_repo = endpoint + '/' + repo
-wikidata_endpoint = "http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql"
-# wikidata_endpoint = 'https://query.wikidata.org/sparql'
-groundtruth_url = 'http://gaiadev01.isi.edu:' + port + '/groundtruth'
-
-# for importing clusters
-import_endpoint = 'http://gaiadev01.isi.edu:7200/repositories'
-repositories = ['gaia0304ta1-test', 'gaia0404ta1-test', 'gaia0404ta1-testing']
-upload_folder = '/nas/home/jchen/gaia-visual-uploads'
-# upload_folder = '/Users/jenniferchen/Documents/AIDA/clusters_upload'
-
+# wikidata_endpoint = "http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql"
+wikidata_endpoint = 'https://query.wikidata.org/sparql'
+store_data = 'store_data'
+repositories = ['gaia0304ta1-test', 'gaia0404ta1-test', 'gaia0404ta1-testing', 'jchen-test']
 username = 'admin'
 password = 'gaia@isi'
+
+# deploy
+# groundtruth_url = 'http://gaiadev01.isi.edu:' + port + '/groundtruth'
+
+# testing
+groundtruth_url = 'http://127.0.0.1:' + port + '/groundtruth'
 
