cad83ff0c89df13a3750ce9de67ae84400e909e2
Shinyi Chen <jchen@isi.edu>
2019-06-17 09:42:11 -0700

    Updated settings.

--- setting.py
+++ setting.py
@@ -1,10 +1,9 @@
 port = '5000'
-endpoint = 'http://gaiadev01.isi.edu:7201/repositories'
+endpoint = 'http://gaiadev01.isi.edu:7200/repositories'
 # wikidata_endpoint = "http://sitaware.isi.edu:8080/bigdata/namespace/wdq/sparql"
 wikidata_endpoint = 'https://query.wikidata.org/sparql'
 store_data = 'store_data'
-# repositories = ['gaia0304ta1-test', 'gaia0404ta1-test', 'gaia0404ta1-testing', 'jchen-test', 'validation-test']
-repositories = ['gaia0416ta2']
+repositories = ['gaia0416ta2', 'gaia0531ta2']
 username = 'admin'
 password = 'gaia@isi'
 
@@ -12,8 +11,8 @@ password = 'gaia@isi'
 url_prefix = ""
 
 # deploy
-# groundtruth_url = 'http://gaiadev01.isi.edu:' + port + '/groundtruth'
+groundtruth_url = 'http://gaiadev01.isi.edu:' + port + '/groundtruth'
 
 # testing
-groundtruth_url = 'http://127.0.0.1:' + port + '/groundtruth'
+# groundtruth_url = 'http://127.0.0.1:' + port + '/groundtruth'
 
